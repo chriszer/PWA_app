e2dac23c1986f862a85dc94b5c46e1d2a006aafa
chriszer tamayo <chriszer.myth@gmail.com>
2021-01-22 13:53:21 +0800

    first commit

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+	//
+	console.log("[Service Worker] Installing Service Worker ...", event);
+});
+self.addEventListener("activate", (event) => {
+	console.log("[Service Worker] Activating Service Worker ...", event);
+});
+
+self.addEventListener("fetch", (event) => {
+	console.log("[Service Worker] Fetching something ...", event);
+	event.respondWith(fetch(event.request));
+});
